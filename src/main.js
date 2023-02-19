7036157b49e2f7b259b83474cc42ef09da37d78e
mrmoda <181909423@163.com>
2023-02-19 19:19:03 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-import { Application } from '@splinetool/runtime';
-
 const canvas = document.getElementById('canvas3d');
 const app = new Application(canvas);
 app.load('https://prod.spline.design/rPgGUJRS4c008BWp/scene.splinecode');
