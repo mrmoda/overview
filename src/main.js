ee3919bd857e0e2d427869146f762201cb014fa7
mrmoda <2545229027@qq.com>
2023-02-19 19:55:53 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,50 +1,5 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import SplineLoader from '@splinetool/loader';
+import { Application } from '@splinetool/runtime';
 
-// camera
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 5, 100000);
-camera.position.set(803.61, 482.95, 1637.88);
-camera.quaternion.setFromEuler(new THREE.Euler(-0.16, 0.44, 0.07));
-
-// scene
-const scene = new THREE.Scene();
-
-// spline scene
-const loader = new SplineLoader();
-loader.load(
-  'https://prod.spline.design/rPgGUJRS4c008BWp/scene.splinecode',
-  (splineScene) => {
-    scene.add(splineScene);
-  }
-);
-
-// renderer
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setAnimationLoop(animate);
-document.body.appendChild(renderer.domElement);
-
-// scene settings
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFShadowMap;
-
-scene.background = new THREE.Color('#d1d0d1');
-renderer.setClearAlpha(1);
-
-// orbit controls
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-controls.dampingFactor = 0.125;
-
-window.addEventListener('resize', onWindowResize);
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
-function animate(time) {
-  controls.update();
-  renderer.render(scene, camera);
-}
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/rPgGUJRS4c008BWp/scene.splinecode');
